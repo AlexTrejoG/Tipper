d78ac6d7abce596a16fb7b07046aec3f36e45263
Alex Trejo <hatrejogalvan18@students.desu.edu>
2020-01-15 18:19:41 -0500

    Initial Commit

---
+++ Tipperoni/Tipperoni/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipperoni
+//
+//  Created by Alex Trejo on 1/15/20.
+//  Copyright © 2020 New Amsterdam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
